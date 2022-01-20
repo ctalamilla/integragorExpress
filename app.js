b44ff82bfbb76b77205a93d134b3ad4de234c2e7
Cristian Patricio Salinas Talamilla <ctalamilla@MBP-de-Cristian.silver.local>
2022-01-20 14:38:25 -0300

    agregue funcion de cargar vistas

--- app.js
+++ app.js
@@ -1,51 +1,33 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
 
 const app = express();
 
 app.listen(3030, ()=> console.log('exitoso'));
 
 
-//app.get('/', (req, res)=> res.send("Bienvenidos al sitio"));
+app.get('/', (req, res)=> res.send("Bienvenidos al sitio"));
 
-app.get('/', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/index.html')
-    res.sendFile(htmlpath)
-})
+testFolder = './views'
 
-app.get('/babbage', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/babbage.html')
-    res.sendFile(htmlpath)
-})
+const vistas = fs.readdirSync(testFolder)
+console.log(vistas)
 
-app.get('/berners-lee', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/berners-lee.html')
-    res.sendFile(htmlpath)
-})
 
-app.get('/clarke', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/clarke.html')
-    res.sendFile(htmlpath)
-})
+function cargarViews (listaVistas){
+    listaVistas.map(archivo=> {
+        let relPath = testFolder + '/' + archivo
+        let URL_ARCHIVO = path.resolve(__dirname, relPath)
+        //console.log(URL_ARCHIVO)
+        let nombreVista = '/'+archivo.split(".")[0]
+        console.log(nombreVista)
+        app.get(nombreVista, (req,res)=> {
+            let htmlpath = path.resolve(URL_ARCHIVO)
+            res.sendFile(htmlpath)
+        })
+    })}
 
-app.get('/hamilton', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/hamilton.html')
-    res.sendFile(htmlpath)
-})
-
-app.get('/hopper', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/hopper.html')
-    res.sendFile(htmlpath)
-})
-
-app.get('/lovelace', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/lovelace.html')
-    res.sendFile(htmlpath)
-})
-
-app.get('/turing', (req,res)=> {
-    let htmlpath = path.resolve(__dirname, './views/turing.html')
-    res.sendFile(htmlpath)
-})
+cargarViews(vistas)
 
 app.use(express.static('public'));
